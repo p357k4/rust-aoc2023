98cbb0e098f95901d09a0eac685e9fb7ba0b1a23
Daniel Lewandowski <daniel.lewandowski@outlook.com>
2023-12-19 13:35:53 +0100

    day 19

---
+++ day19/src/main.rs
@@ -0,0 +1,181 @@
+mod main_test;
+
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufReader, Read};
+use itertools::{Itertools};
+use nom::branch::alt;
+use nom::bytes::complete::tag;
+use nom::character::complete;
+use nom::character::complete::{alpha1, alphanumeric0, alphanumeric1};
+use nom::combinator::opt;
+use nom::IResult;
+use nom::multi::separated_list0;
+use nom::sequence::tuple;
+
+#[derive(Clone, Eq, PartialEq)]
+struct Part {
+    v: HashMap<String, u32>,
+}
+
+struct Condition {
+    category: String,
+    operator: String,
+    value: u32,
+}
+
+struct Rule {
+    condition: Option<Condition>,
+    to: String,
+}
+
+struct Workflow {
+    name: String,
+    rules: Vec<Rule>,
+}
+
+struct Input {
+    workflows: Vec<Workflow>,
+    parts: Vec<Part>,
+}
+
+fn parse_attribute(input: &str) -> IResult<&str, (&str, u32)> {
+    let r = tuple((alphanumeric0, tag("="), complete::u32))(input);
+
+    r.map(|(leftover, (name, _, value))| {
+        (leftover, (name, value))
+    })
+}
+
+fn parse_part(input: &str) -> IResult<&str, Part> {
+    let r = tuple((tag("{"), separated_list0(tag(","), parse_attribute), tag("}")))(input);
+
+    r.map(|(leftover, (_, vec, _))| {
+        let mut v = HashMap::new();
+
+        for t in vec {
+            v.insert(t.0.to_string(), t.1);
+        }
+
+        (leftover, Part { v })
+    })
+}
+
+fn parse_condition(input: &str) -> IResult<&str, Condition> {
+    let r = tuple((alpha1, alt((tag(">"), tag("<"))), complete::u32))(input);
+
+    r.map(|(leftover, (cat, op, v))| {
+        (
+            leftover,
+            Condition {
+                category: cat.to_string(),
+                operator: op.to_string(),
+                value: v,
+            }
+        )
+    })
+}
+
+fn parse_rule(input: &str) -> IResult<&str, Rule> {
+    tuple((opt(tuple((parse_condition, tag(":")))), alphanumeric1))(input)
+        .map(|(leftover, (opt, to))| {
+            (leftover, Rule { condition: opt.map(|v| v.0), to: to.to_string() })
+        })
+}
+
+fn parse_workflow(input: &str) -> IResult<&str, Workflow> {
+    tuple((alphanumeric1, tag("{"), separated_list0(tag(","), parse_rule), tag("}")))(input)
+        .map(|(leftover, (name, _, rules, _))| {
+            (leftover, Workflow { name: name.to_string(), rules: rules })
+        })
+}
+
+fn load(path: &str) -> Result<Input, Box<dyn std::error::Error>> {
+    let file = File::open(path)?;
+    let mut reader = BufReader::new(file);
+
+    let mut content = String::new();
+
+    reader.read_to_string(&mut content);
+
+    let Some((workflows_str, parts_str)) = content.split_once("\n\n") else { todo!() };
+
+    let workflows = workflows_str.split("\n").map(|line|
+        {
+            let Ok((leftover, workflow)) = parse_workflow(line) else { todo!() };
+
+            workflow
+        }).collect_vec();
+
+    let parts = parts_str.split("\n").map(|line|
+        {
+            let Ok((leftover, part)) = parse_part(line) else { todo!() };
+
+            part
+        }).collect_vec();
+
+    Ok(Input { workflows, parts })
+}
+
+fn part1(path: &str) -> Result<u32, Box<dyn std::error::Error>> {
+    let input = load(path)?;
+
+    let mut result = 0;
+    for part in &input.parts {
+        let accepted = analyze(&input.workflows, part, &"in".into());
+        if accepted {
+            let delta: u32= part.v.values().sum();
+            result += delta;
+        }
+    }
+
+    Ok(result)
+}
+
+fn analyze(workflows: &Vec<Workflow>, part: &Part, name: &String) -> bool {
+    let Some(workflow) = workflows.iter().find_or_first(|w| w.name == *name) else { todo!() };
+
+    let rule_option = workflow.rules.iter().find(|rule| {
+        match &rule.condition {
+            Some(condition) if condition.operator == ">" && part.v[&condition.category] > condition.value => {
+                true
+            },
+
+            Some(condition) if condition.operator == "<" && part.v[&condition.category] < condition.value => {
+                true
+            },
+
+            Some(_) => {
+                false
+            },
+
+            None => {true},
+        }
+    });
+
+    match rule_option {
+        Some(rule) if rule.to == "A" => {
+            true
+        }
+        Some(rule) if rule.to == "R" => {
+            false
+        }
+        Some(rule) => {
+            analyze(workflows, part, &rule.to)
+        }
+        None => {
+            todo!()
+        }
+    }
+}
+
+fn part2(path: &str) -> Result<usize, Box<dyn std::error::Error>> {
+    let input = load(path)?;
+
+    let result = 0;
+    Ok(result)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    Ok(())
+}
---
+++ day19/src/main_test.rs
@@ -0,0 +1,28 @@
+#[cfg(test)]
+mod tests {
+    use crate::{part1, part2};
+
+    #[test]
+    fn part1_example() {
+        let result = part1("../example/day19_part1_example.txt").unwrap();
+        assert_eq!(result, 19114);
+    }
+
+    #[test]
+    fn part1_full() {
+        let result = part1("../data/day19_part1.txt").unwrap();
+        assert_eq!(result, 350678);
+    }
+
+    #[test]
+    fn part2_example1() {
+        let result = part2("../example/day19_part1_example.txt").unwrap();
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn part2_full() {
+        let result = part2("../data/day19_part1.txt").unwrap();
+        assert_eq!(result, 0);
+    }
+}
